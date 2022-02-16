238b9133abd59adf808481e18abe4de02ccda2d8
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-02-16 18:00:01 -0300

    feat: api Stripe integration and authentication with Next Auth

--- src/components/SignInButton/SignInButton.tsx
+++ src/components/SignInButton/SignInButton.tsx
@@ -1,26 +1,32 @@
 import { FaGithub } from 'react-icons/fa';
-import { FiX } from 'react-icons/fi'
+import { FiX } from 'react-icons/fi';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
-import styles from './styles.module.scss'
+import styles from './styles.module.scss';
 
 export function SignInButton() {
-    const isUserLoggedIn = true;
 
-    return isUserLoggedIn 
+    const {data: session} = useSession();
+
+    return session 
     ? (
         <button 
             type="button"
             className={styles.signInButton}
         >
             <FaGithub color='#04d361' />
-            user name
-            <FiX  color='#737388'/>
+            {session.user.name}
+            <FiX 
+                color='#737388'
+                onClick={() => signOut()}
+            />
         </button>
     ) 
     : (
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
             <FaGithub color='#eba417' />
             Sign in with Github
