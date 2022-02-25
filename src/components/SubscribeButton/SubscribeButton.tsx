dbe1201c0ce467cdac7ed4b47ff6c444242a64ad
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-02-25 17:59:11 -0300

    feat: enable payment through Stripe's Checkout Session

--- src/components/SubscribeButton/SubscribeButton.tsx
+++ src/components/SubscribeButton/SubscribeButton.tsx
@@ -1,14 +1,43 @@
-import styles from './styles.module.scss'
+import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJS } from '../../services/stripe-js';
+
+import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
 export function SubscribeButton(priceId: SubscribeButtonProps) {
+
+    const {data: session} = useSession();
+
+    async function handleSubscribeButton() {
+        if (!session) {
+            signIn('github')
+
+            return;
+        }
+
+        try{
+            const response = await api.post('/subscribe');
+
+            const { sessionId } = await response.data;
+
+            const stripe = await getStripeJS();
+
+            await stripe.redirectToCheckout({ sessionId });
+            
+        } catch (err) {
+            alert(err.message);
+        }
+    }
+
     return (
         <button
             type="button"
             className={styles.subscribeButton}
+            onClick={handleSubscribeButton}
         >
             Subscribe now
         </button>
