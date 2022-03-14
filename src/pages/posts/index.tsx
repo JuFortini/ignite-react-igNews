e091664a9576d2b998568322cd68e263464ecc82
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-03-14 14:13:59 -0300

    perf: improvements

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from "next";
 import Head from "next/head";
 import Prismic from "@prismicio/client";
 import { RichText } from 'prismic-dom';
+import Link from "next/link";
 
 import { getPrismicClient } from "../../services/prismic";
 
@@ -28,11 +29,13 @@ export default function Posts({ posts }: PostProps) {
             <main className={styles.container}>
                 <div className={styles.posts}>
                     {posts.map(post => 
-                        <a key={post.slug} href="#">
-                            <time>{post.updatedAt}</time>
-                            <strong>{post.title}</strong>
-                            <p>{post.excerpt}</p>
-                        </a>
+                        <Link key={post.slug} href={`/posts/${post.slug}`}>
+                            <a>
+                                <time>{post.updatedAt}</time>
+                                <strong>{post.title}</strong>
+                                <p>{post.excerpt}</p>
+                            </a>
+                        </Link>
                     )}
                 </div>
             </main>
@@ -66,6 +69,7 @@ export const getStaticProps: GetStaticProps = async () => {
     return {
         props: {
             posts
-        }
+        },
+        revalidate: 60 * 60 * 24,  // 24 hours
     }
 }
