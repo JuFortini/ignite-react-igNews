383763479553006cd9406cfcbe10f36e6f7f1a78
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-08-04 17:09:17 -0300

    feat: test library config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules/", "/.next/"], // files that will be ignored
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"], // documents that will execute before test
+  transform: {
+    "^.+\\.(js|ts|jsx|tsx)$": "<rootDir>/node_modules/babel-jest"  // extensions that will be translated by babel-jest
+  },
+  testEnvironment: 'jsdom',  // enviroment that the test will be executed so it can knows how to behave
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,6 @@ import { AppProps } from 'next/app';
 
 import { Header } from '../components/Header/Header';
 
-
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
