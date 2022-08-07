8015c6d507739a10ae1faf0e5d4df474c080ae74
Jucimara Fortini <jucimaraafortini@gmail.com>
2022-08-07 16:41:37 -0300

    feat: Posts page tests - getStaticProps test included

---
+++ src/tests/pages/Posts.spec.tsx
@@ -0,0 +1,55 @@
+import { render, screen } from '@testing-library/react';
+import Posts, { getStaticProps } from '../../pages/posts';
+import { getPrismicClient } from '../../services/prismic';
+
+const posts = {
+  slug: 'my-fake-post',
+  title: 'My Fake Post',
+  excerpt: 'my-fake-post-excerpt',
+  updatedAt: '07 de agosto de 2022',
+}
+
+jest.mock('../../services/prismic');
+
+describe('Posts page', () => {
+  it('renders correctly', () => {
+    render(<Posts posts={[posts]} />)
+
+    expect(screen.getByText('My Fake Post')).toBeInTheDocument();
+  })
+
+  it('loads initial data', async () => {
+    const prismicClientMocked = jest.mocked(getPrismicClient);
+    const queryPrismicClientMocked = jest.fn();
+    prismicClientMocked.mockReturnValueOnce({
+      query: queryPrismicClientMocked.mockResolvedValueOnce({
+        results: [
+          {
+            uid: 'my-fake-post',
+            data: {
+              title: [
+                { type: 'heading', text: 'My Fake Post' }
+              ],
+              content: [
+                { type: 'paragraph', text: 'my-fake-post-excerpt' }
+              ],
+            },
+            last_publication_date: '08-07-2022',
+          }
+        ]
+      })
+    } as any)
+
+    const response = await getStaticProps({});
+
+    console.log(response);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          posts: [posts]
+        }
+      })
+    )
+  })
+})
